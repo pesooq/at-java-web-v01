f1b8094f86f06eaf814b9274f3c2d8b74f61a66b
Vladimir <athomze@mail.ru>
2025-12-17 22:17:20 +0300

    ДЗ урока 01: переделан тест 01-02 и 01-03 поиск Фуллера

--- src/test/java/work/part01/SimpleWikiTest.java
+++ src/test/java/work/part01/SimpleWikiTest.java
@@ -1,8 +1,9 @@
 package work.part01;
 
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 //import org.junit.jupiter.api.BeforeEach;
 //import org.junit.jupiter.api.AfterEach;
 //import org.junit.jupiter.api.DisplayName;
@@ -17,7 +18,13 @@ public class SimpleWikiTest {
     @Test
     void test02() {
         open("https://ru.wikipedia.org/wiki/Selenium");
-        $("body").shouldHave(text("Selenide"));
+        $("body").shouldHave(text("Selenium"));
+        //sleep(2000);
+    }
+    @Test
+    void test03() {
+        open("https://slqa.ru/cases/SimpleForm");
+        $("body").shouldHave(text("Томас Фуллер"));
         //sleep(2000);
     }
 }
